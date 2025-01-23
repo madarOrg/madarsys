db5499de723a9409d3799285cd8f52b3a795a32f
lamya <alhemyarilamya@gmail.com>
2025-01-23 11:30:28 -0800

    all pro

---
+++ public/build/assets/soft-ui-dashboard-tailwind-D3acPGgT.js
@@ -0,0 +1,17 @@
+/*!
+
+=========================================================
+* Soft UI Dashboard Tailwind - v1.0.5
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/soft-ui-dashboard-tailwind
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (site.license)
+
+* Coded by www.creative-tim.com
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/window.location.pathname.split("/").pop().split(".")[0];var i=window.location.pathname.split("/"),e=i.includes("pages")?"../":"./";window.location.pathname.split("/");i.includes("pages");c(e+"resources/css/perfect-scrollbar.css");t(e+"assets/js/perfect-scrollbar.js",!0);document.querySelector("nav [navbar-trigger]")&&t(e+"assets/js/navbar-collapse.js",!0);document.querySelector("[data-target='tooltip']")&&(t(e+"assets/js/tooltips.js",!0),c(e+"resources/css/tooltips.css"));document.querySelector("[nav-pills]")&&t(e+"assets/js/nav-pills.js",!0);document.querySelector("[dropdown-trigger]")&&t(e+"assets/js/dropdown.js",!0);document.querySelector("[fixed-plugin]")&&t(e+"assets/js/fixed-plugin.js",!0);document.querySelector("[navbar-main]")&&(t(e+"assets/js/sidenav-burger.js",!0),t(e+"assets/js/navbar-sticky.js",!0));document.querySelector("canvas")&&(t(e+"assets/js/chart-1.js",!0),t(e+"assets/js/chart-2.js",!0));function t(a,s){let r=document.createElement("script");r.setAttribute("src",a),r.setAttribute("type","text/javascript"),r.setAttribute("async",s),document.head.appendChild(r)}function c(a){let s=document.createElement("link");s.setAttribute("href",a),s.setAttribute("type","text/css"),s.setAttribute("rel","stylesheet"),document.head.appendChild(s)}
